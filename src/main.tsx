aa7411930a4541ab8bae32212ab43c2cede6e2ee
gnimdoubodjona <gnimdoubodjona@gmail.com>
2025-08-27 03:04:41 +0200

    Configuration de convex-auth

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import {BrowserRouter} from "react-router-dom";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <BrowserRouter>
-          <App />
+          <ConvexProvider client={convex}>
+              <App />
+          </ConvexProvider>
       </BrowserRouter>
   </StrictMode>,
 )
